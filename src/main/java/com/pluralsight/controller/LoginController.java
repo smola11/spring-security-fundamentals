4122978b579cc662a230fc634bd8c4898c23869f
Smolarz <mjsz@gft.com>
2018-07-30 11:37:05 +0200

    Added LoginController

---
+++ src/main/java/com/pluralsight/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+		System.out.println("in the login method");
+
+		// This tells InternalResourceViewResolver inside servlet-config to that we want
+		// ot look to WEB-INF/jsp directory for a ".jsp". This is going to look for
+		// login.jsp;
+		return "login";
+	}
+}
