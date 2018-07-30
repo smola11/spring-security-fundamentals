defd70887efe4eaa84c5fb1b74eba2972708f00a
Smolarz <mjsz@gft.com>
2018-07-30 11:18:17 +0200

    Added test for BCrypt hashing and change password encoder to bcrypt

--- src/test/java/PasswordHash.java
+++ src/test/java/PasswordHash.java
@@ -1,8 +1,16 @@
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import junit.framework.TestCase;
 
 public class PasswordHash extends TestCase {
+
+	public void testBCryptHash() {
+		String password = "secret";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+		System.out.println(hashedPassword);
+	}
 
 	public void testMD5Hash() {
 		String password = "secret";
